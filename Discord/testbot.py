9d8aab5c668d716345a15d4c939fa1e96e3a18a5
HiBixby <fpssj@naver.com>
2022-05-30 01:30:16 +0900

    settings

--- Discord/testbot.py
+++ Discord/testbot.py
@@ -1,6 +1,14 @@
-import os
-from dotenv import load_dotenv
+import discord
+from discord.ext import commands
+from Discord.to import Token
 
-load_dotenv()
+bot=commands.Bot(command_prefix='./')
 
-token = os.getenv("TOKEN")
+@bot.event
+async def on_ready():
+    print('로그인중입니다. ')
+    print(f"봇={bot.user.name}로 연결중")
+    print('연결이 완료되었습니다.')
+    await bot.change_presence(status=discord.Status.online, activity=None)
+    
+bot.run(Token)#보안을위해 다른 코드(to.py)에서 토큰값을 가져옴.
