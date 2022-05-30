b72a970f926d1c2439daa306f0e2a5ae563e261b
HiBixby <fpssj@naver.com>
2022-05-30 15:54:00 +0900

    embed update

--- Discord/testbot.py
+++ Discord/testbot.py
@@ -10,5 +10,29 @@ async def on_ready():
     print(f"봇={bot.user.name}로 연결중")
     print('연결이 완료되었습니다.')
     await bot.change_presence(status=discord.Status.online, activity=None)
-    
+
+@bot.command()
+async def 안녕(ctx):
+    await ctx.channel.send('나도 안녕!')
+
+@bot.command()
+async def 공지(ctx):
+    embed=discord.Embed(title="상명라운지 | 통합공지 게시판목록 | 상명라운지", url="https://www.smu.ac.kr/lounge/notice/notice.do?mode=list&&articleLimit=10&srUpperNoticeYn=on&article.offset=0", description="통합공지 조회", color=0x2e35ff)
+    embed.set_author(name="상명대학교 알리미 슴우")
+    embed.set_thumbnail(url="https://www.smu.ac.kr/favicon.ico")
+    embed.add_field(name="undefined", value="undefined", inline=False)
+    embed.add_field(name="undefined", value="undefined", inline=False)
+    embed.add_field(name="undefined", value="undefined", inline=False)
+    embed.add_field(name="undefined", value="undefined", inline=False)
+    embed.add_field(name="undefined", value="undefined", inline=False)
+    embed.add_field(name="undefined", value="undefined", inline=False)
+    embed.add_field(name="undefined", value="undefined", inline=False)
+    embed.add_field(name="undefined", value="undefined", inline=False)
+    embed.add_field(name="undefined", value="undefined", inline=False)
+    embed.add_field(name="undefined", value="undefined", inline=False)
+    embed.set_footer(text="1 페이지")
+    await ctx.send(embed=embed)
+
+
+
 bot.run(Token)#보안을위해 다른 코드(to.py)에서 토큰값을 가져옴.
